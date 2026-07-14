36f1163dc6c502f3169ab0ad676c504b9ec9be6b
befkad ayele <befebefekad@gmail.com>
2026-07-15 02:07:13 +0300

    complete day 02 practice

---
+++ practice.py
@@ -0,0 +1,29 @@
+temp =10
+if temp <15:
+    print("cold")
+elif temp<28:
+    print("warm")  
+else: 
+    print("hot") 
+
+
+# recipt= 1
+# while recipt <= 10:
+#     print(f"RECIPT #{recipt}")
+#     recipt += 1
+
+
+number=1
+while number <= 20:
+
+    if number
+        print(number)
+    else:
+        number += 1
+
+
+        #print
+        print("hello python")
+        # variabele
+        a=7
+        print(a)
